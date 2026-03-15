97889675950ea45c96f262566ea4aabe929efa2c
Chua Chloe <chloechua18@gmail.com>
2026-03-15 21:12:44 +0800

    Add telegram handle support to person storage

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -15,6 +15,7 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.TelegramHandle;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -28,19 +29,24 @@ class JsonAdaptedPerson {
     private final String phone;
     private final String email;
     private final String address;
+    private final String telegramHandle;
     private final List<JsonAdaptedTag> tags = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonAdaptedPerson} with the given person details.
      */
     @JsonCreator
-    public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
-            @JsonProperty("email") String email, @JsonProperty("address") String address,
-            @JsonProperty("tags") List<JsonAdaptedTag> tags) {
+    public JsonAdaptedPerson(@JsonProperty("name") String name,
+                             @JsonProperty("phone") String phone,
+                             @JsonProperty("email") String email,
+                             @JsonProperty("address") String address,
+                             @JsonProperty("telegramHandle") String telegramHandle,
+                             @JsonProperty("tags") List<JsonAdaptedTag> tags) {
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.telegramHandle = telegramHandle;
         if (tags != null) {
             this.tags.addAll(tags);
         }
@@ -54,6 +60,7 @@ class JsonAdaptedPerson {
         phone = source.getPhone().value;
         email = source.getEmail().value;
         address = source.getAddress().value;
+        telegramHandle = source.getTelegramHandle() == null ? null : source.getTelegramHandle().value;
         tags.addAll(source.getTags().stream()
                 .map(JsonAdaptedTag::new)
                 .collect(Collectors.toList()));
@@ -102,8 +109,18 @@ class JsonAdaptedPerson {
         }
         final Address modelAddress = new Address(address);
 
+        final TelegramHandle modelTelegramHandle;
+        if (telegramHandle == null) {
+            modelTelegramHandle = null;
+        } else {
+            if (!TelegramHandle.isValidTelegramHandle(telegramHandle)) {
+                throw new IllegalValueException(TelegramHandle.MESSAGE_CONSTRAINTS);
+            }
+            modelTelegramHandle = new TelegramHandle(telegramHandle);
+        }
+
         final Set<Tag> modelTags = new HashSet<>(personTags);
-        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags);
+        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTelegramHandle, modelTags);
     }
 
 }
