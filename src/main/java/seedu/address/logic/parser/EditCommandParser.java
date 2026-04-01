c4760e519ec71b026196384b5c4c78199ebd114c
calijacked <162989170+calijacked@users.noreply.github.com>
2026-04-01 17:28:19 +0800

    Remove tag references in EditCommand and EditCommandParser

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -2,12 +2,9 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_COURSE_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_GENERAL_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM_HANDLE;
 
 import java.util.Collection;
@@ -36,7 +33,7 @@ public class EditCommandParser implements Parser<EditCommand> {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
-                        PREFIX_TELEGRAM_HANDLE, PREFIX_ROLE_TAG, PREFIX_COURSE_TAG, PREFIX_GENERAL_TAG);
+                        PREFIX_TELEGRAM_HANDLE);
 
         Index index;
 
@@ -63,13 +60,7 @@ public class EditCommandParser implements Parser<EditCommand> {
             editPersonDescriptor.setTelegramHandle(ParserUtil.parseTelegramHandle(argMultimap
                     .getValue(PREFIX_TELEGRAM_HANDLE).get()));
         }
-        parseTagsForEdit(argMultimap.getAllValues(PREFIX_ROLE_TAG), TagType.ROLE)
-                .ifPresent(editPersonDescriptor::setRoleTags);
-        parseTagsForEdit(argMultimap.getAllValues(PREFIX_COURSE_TAG), TagType.COURSE)
-                .ifPresent(editPersonDescriptor::setCourseTags);
-        parseTagsForEdit(argMultimap.getAllValues(PREFIX_GENERAL_TAG), TagType.GENERAL)
-                .ifPresent(editPersonDescriptor::setGeneralTags);
-
+        
         if (!editPersonDescriptor.isAnyFieldEdited()) {
             throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
         }
